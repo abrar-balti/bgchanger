5c1ac9a200c321eb58a7962ee894c9b8221efac4
abrarbalti <abrarbulti@gmail.comgit config --global user.email abrarbulti@gmail.comgit config --global user.email abrarbulti@gmail.com>
2025-05-25 16:01:22 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,93 @@
+import { useState } from "react";
+
+function App() {
+  const [color, seColor] = useState("olive");
+  const date = Date.now();
+  return (
+    <>
+      <div
+        className="w-full h-screen flex flex-col justify-between items-center p-10"
+        style={{ backgroundColor: color }}
+      >
+        <div>
+          <h1>Select the Color you love</h1>
+          <h3>Date: {date}</h3>
+        </div>
+        <div className="btn-parent bg-white w-fit p-4 rounded-full flex  gap-2">
+          <button
+            className=" px-4 py-2 rounded-full text-white cursor-pointer"
+            style={{ backgroundColor: "red" }}
+            onClick={() => seColor("red")}
+          >
+            Red
+          </button>
+          <button
+            className=" px-4 py-2 rounded-full text-white cursor-pointer"
+            style={{ backgroundColor: "blue" }}
+            onClick={() => seColor("blue")}
+          >
+            Blue
+          </button>
+          <button
+            className=" px-4 py-2 rounded-full text-white cursor-pointer"
+            style={{ backgroundColor: "green" }}
+            onClick={() => seColor("green")}
+          >
+            Green
+          </button>
+          <button
+            className=" px-4 py-2 rounded-full text-white cursor-pointer"
+            style={{ backgroundColor: "orange" }}
+            onClick={() => seColor("orange")}
+          >
+            Orange
+          </button>
+          <button
+            className=" px-4 py-2 rounded-full text-white cursor-pointer"
+            style={{ backgroundColor: "pink" }}
+            onClick={() => seColor("pink")}
+          >
+            Pink
+          </button>
+          <button
+            className=" px-4 py-2 rounded-full text-white cursor-pointer"
+            style={{ backgroundColor: "purple" }}
+            onClick={() => seColor("Purple")}
+          >
+            Purple
+          </button>
+          <button
+            className="px-4 py-2 rounded-full text-white cursor-pointer"
+            style={{ backgroundColor: "teal" }}
+            onClick={() => seColor("teal")}
+          >
+            Teal
+          </button>
+          <button
+            className="px-4 py-2 rounded-full text-white cursor-pointer"
+            style={{ backgroundColor: "goldenrod" }}
+            onClick={() => seColor("goldenrod")}
+          >
+            Goldenrod
+          </button>
+          <button
+            className="px-4 py-2 rounded-full text-white cursor-pointer"
+            style={{ backgroundColor: "crimson" }}
+            onClick={() => seColor("crimson")}
+          >
+            Crimson
+          </button>
+          <button
+            className="px-4 py-2 rounded-full text-white cursor-pointer"
+            style={{ backgroundColor: "dodgerblue" }}
+            onClick={() => seColor("dodgerblue")}
+          >
+            Dodger Blue
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
